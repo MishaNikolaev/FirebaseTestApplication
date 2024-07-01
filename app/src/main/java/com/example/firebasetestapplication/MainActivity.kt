7096ad7b977bcac94f4f693ff97cbcd84a3faf8f
Mikhail Nikolaev <nmichail154@mail.ru>
2024-07-01 19:41:16 +0700

    Read data from firestore

--- app/src/main/java/com/example/firebasetestapplication/MainActivity.kt
+++ app/src/main/java/com/example/firebasetestapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firebasetestapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,17 +13,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firebasetestapplication.data.Book
 import com.example.firebasetestapplication.ui.theme.FirebaseTestApplicationTheme
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.toObjects
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
@@ -35,13 +42,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("RememberReturnType")
 @Composable
 fun MainScreen() {
 
     val fs = Firebase.firestore
+    val list = remember{
+        mutableStateOf(emptyList<Book>())
+    }
+
+    val listener = fs.collection("books").addSnapshotListener{ snapshot, exeption ->
+        list.value = snapshot?.toObjects(Book::class.java)!!
+    }
+
+    //listener.remove()
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(bottom = 50.dp),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 50.dp),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         LazyColumn(
@@ -49,10 +68,21 @@ fun MainScreen() {
                 .fillMaxWidth()
                 .fillMaxHeight(0.8f)
         ) {
-
+            items(list.value){ book ->
+                Card(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp, end = 10.dp,
+                        top = 50.dp)){
+                    Text(text = book.name,
+                        modifier = Modifier.fillMaxWidth()
+                            .wrapContentWidth().padding(15.dp))
+                }
+            }
         }
         Spacer(modifier = Modifier.height(10.dp))
-        Button(modifier = Modifier.fillMaxWidth().padding(10.dp),
+        Button(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
             onClick = {
             fs.collection("books").document().set(Book(
                 "Cloud of war",
